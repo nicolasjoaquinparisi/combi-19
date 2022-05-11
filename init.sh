a240906690c6bdb5a39804d5e002dd68e6b20d94
Nicolás Joaquín Parisi <nicojp.1808@gmail.com>
2022-05-11 08:49:37 -0300

    ajuste al init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,6 @@ pip install psycopg2
 pip install pytz
 python -m pip install django-browser-reload
 
-python -m pip install django-tailwind
-
 docker-compose up
 
 python manage.py makemigrations combiapp
