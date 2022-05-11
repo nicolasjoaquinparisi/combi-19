66ab02cc6a927bb8b60c2299bbeb4d1ba0a19495
Nicolás Joaquín Parisi <nicojp.1808@gmail.com>
2022-05-11 02:33:50 -0300

    dockerización de postgresql

--- sc19/settings.py
+++ sc19/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'combiapp',
     'clearcache',
+    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware"
 ]
 
 ROOT_URLCONF = 'sc19.urls'
